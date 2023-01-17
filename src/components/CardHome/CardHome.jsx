a6817932591ed297be703fecce34969199f245e4
Marc España Quinquilla <44228378+Marc1515@users.noreply.github.com>
2023-01-17 20:41:14 +0100

    Home retocada y presentación añadida

--- src/components/CardHome/CardHome.jsx
+++ src/components/CardHome/CardHome.jsx
@@ -1,23 +1,16 @@
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
-import { Typography, CardActionArea } from '@mui/material';
-
 import CardsHome from './CardsHome';
 import './CardHome.css';
 
 const CardHome = () => {
 	return (
 		<>
-			<Typography variant='h1' component='h1' className='title-cards'>
-				Grammar
-			</Typography>
 			<div className='container-wrapper-card'>
 				{CardsHome.map((card) => (
 					<a href={card.url} key={card.titulo}>
 						<Card className='wrapper-card'>
-							<CardActionArea>
-								<CardMedia component='img' height='120' image={card.image} />
-							</CardActionArea>
+							<CardMedia component='img' height='120' image={card.image} />
 						</Card>
 					</a>
 				))}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,26 @@
 import CardHome from '../../components/CardHome/CardHome';
+import { Typography } from '@mui/material';
 import './HomePage.css';
 
 const HomePage = () => {
-	return <CardHome />;
+	return (
+		<>
+			<Typography variant='h1' component='h1' className='title-cards'>
+				Grammar
+			</Typography>
+			<Typography variant='subtitle2' className='text-grammar'>
+				¡Bienvenido a nuestra aplicación de aprendizaje de inglés!
+			</Typography>
+			<Typography variant='subtitle2' className='text-grammar'>
+				Con nuestra aplicación, podrás mejorar tus habilidades de inglés de
+				manera eficiente y divertida. Ofrecemos una variedad de exámenes que
+				duran aproximadamente 1 minuto para aprender o refrescar tus
+				conocimientos, diseñados para ayudarte a mejorar tu vocabulario y
+				gramática.
+			</Typography>
+			<CardHome />;
+		</>
+	);
 };
 
 export { HomePage };
