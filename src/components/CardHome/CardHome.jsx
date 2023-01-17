c4e135944875ed7212d49b875116e81294f8d4fc
Marc España Quinquilla <44228378+Marc1515@users.noreply.github.com>
2023-01-17 12:02:42 +0100

    Nuevas imágenes para las cartas de la Home añadidas, tambien se les ha pusto un background

--- src/components/CardHome/CardHome.jsx
+++ src/components/CardHome/CardHome.jsx
@@ -1,20 +1,23 @@
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardsHome from './CardsHome';
+import './CardHome.css';
 
 const CardHome = () => {
 	return (
 		<>
 			<Typography variant='h1' component='h1' className='title-cards'>
-				Contenido
+				Grammar
 			</Typography>
 			<div className='container-wrapper-card'>
 				{CardsHome.map((card) => (
 					<div className='wrapper-card' key={card.titulo}>
 						<Card>
 							<CardContent>
-								<a href={card.url}>{card.titulo}</a>
+								<a href={card.url}>
+									<Box component='img' src={card.image} />
+								</a>
 							</CardContent>
 						</Card>
 					</div>
--- src/components/CardHome/CardsHome.js
+++ src/components/CardHome/CardsHome.js
@@ -2,26 +2,32 @@ const CardsHome = [
 	{
 		titulo: 'Present Simple',
 		url: '/#/present-simple',
+		image: './src/assets/presentSimpleCard.png',
 	},
 	{
 		titulo: 'Past Simple',
 		url: '/#/past-simple',
+		image: './src/assets/pastSimpleCard.png',
 	},
 	{
 		titulo: 'Present Continuous',
 		url: '/#/present-countinuous',
+		image: './src/assets/presentContinuousCard.png',
 	},
 	{
 		titulo: 'Future Simple',
 		url: '/#/future-simple',
+		image: './src/assets/futureSimpleCard.png',
 	},
 	{
 		titulo: 'Irregular Verbs',
 		url: '/#/irregular-verbs',
+		image: './src/assets/irregularVerbsCard.png',
 	},
 	{
 		titulo: 'Phrasal Verbs',
 		url: '/#/phrasal-verbs',
+		image: './src/assets/phrasalVerbsCard.png',
 	},
 ];
 
